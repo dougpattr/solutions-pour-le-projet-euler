444d2c414b86f25dc3cff58a8761c17952000351
Doug Pattr <dougpattr@outlook.com>
2018-06-08 01:05:09 -0300

    finale

---
+++ qodlr.h
@@ -0,0 +1,10 @@
+// qodlr.h : arquivo de inclusão para inclusões de problemas do project euler
+//
+
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+#define ERROR "ERRO NOS DADOS DE ENTRADA, TECLE ALGO PARA CONTINUAR"
+#define SPACE "\n\n"
+
+#define PROBLEM001 "ENCONTRE A SOMA DE TODOS OS MÚLTIPLOS DE 3 OU 5 ABAIXO DE 1000"
